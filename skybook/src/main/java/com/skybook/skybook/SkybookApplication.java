e050684c699bc5327df96dd0aca12ab01c3b7c53
sky107 <69970001+sky107@users.noreply.github.com>
2022-08-23 22:27:04 +0530

    Add files via upload

---
+++ skybook/src/main/java/com/skybook/skybook/SkybookApplication.java
@@ -0,0 +1,13 @@
+package com.skybook.skybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkybookApplication.class, args);
+	}
+
+}
